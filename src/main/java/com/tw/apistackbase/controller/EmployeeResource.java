d92c459490c7a655a2966534903b9f6fdc9e311e
sdp <403213485@qq.com>
2019-08-28 15:38:49 +0800

    2

--- src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -1,10 +1,13 @@
 package com.tw.apistackbase.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.omg.PortableServer.ID_ASSIGNMENT_POLICY_ID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +28,8 @@ public class EmployeeResource {
 	
 	List<Employee> employees = new ArrayList<Employee>();
 	
+	
+	//查询所有
 	@GetMapping(path ="/")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Employee> getEmployee(){
@@ -35,6 +40,7 @@ public class EmployeeResource {
 		return employees;
 	}
 	
+	//添加
 	@PostMapping(path ="/")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Employee creatEmployee(@RequestBody Employee employee) {
@@ -42,6 +48,7 @@ public class EmployeeResource {
 			return employee;
 	}
 	
+	//查询某个ID
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<Employee> getOneEmployee(@PathVariable String id){
 		for(Employee employee:employees) {
@@ -51,5 +58,37 @@ public class EmployeeResource {
 		}
 		return ResponseEntity.notFound().build();
 	}
+	
+	//修改
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable String id,@RequestBody Employee changeEmployee){
+		for(Employee employee:employees) {
+			if(employee.getID().equals(id)) {
+				employee.setID(changeEmployee.getID());
+				employee.setName(changeEmployee.getName());
+				employee.setAge(changeEmployee.getAge());
+				employee.setGender(changeEmployee.getGender());
+				return ResponseEntity.ok(employee);
+			}
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	
+	
+	//删除
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<String> deletEmployee(@PathVariable String id) {
+		Iterator<Employee> iterator = employees.iterator();
+		  while (iterator.hasNext()) {
+			   if(iterator.next().getID() == id) {
+			    iterator.remove();
+			    return ResponseEntity.ok(id + "");
+			   }
+
+			}
+		  return ResponseEntity.notFound().build();
+	}
+	
 	
 }
